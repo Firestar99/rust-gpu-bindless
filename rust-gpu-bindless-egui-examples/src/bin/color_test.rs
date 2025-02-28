7de05d0f10eb5b6f776a9c4ac5021efe52810421
Firestar99 <firestar99@sydow.cloud>
2025-02-28 17:13:28 +0100

    egui: example color_test, add vertical ScrollArea

--- rust-gpu-bindless-egui-examples/src/bin/color_test.rs
+++ rust-gpu-bindless-egui-examples/src/bin/color_test.rs
@@ -1,10 +1,11 @@
 //! egui_demo_app example from egui
 
+use egui::ScrollArea;
 use rust_gpu_bindless_egui_examples::run;
 
 pub fn main() {
 	let mut demo = egui_demo_lib::ColorTest::default();
 	run(move |ctx| {
-		egui::CentralPanel::default().show(&ctx, |ui| demo.ui(ui));
+		egui::CentralPanel::default().show(&ctx, |ui| ScrollArea::vertical().show(ui, |ui| demo.ui(ui)));
 	});
 }
