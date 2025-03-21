e6364673e8ba16ef6121d3a2c6bd8c26fd533ee5
Firestar99 <firestar99@sydow.cloud>
2025-03-21 19:06:13 +0100

    project: setup workspace Cargo.toml, dependencies and paths

--- crates/egui/build.rs
+++ crates/egui/build.rs
@@ -1,8 +1,7 @@
+use rust_gpu_bindless_shader_builder::ShaderSymbolsBuilder;
 use std::error::Error;
 
-use space_engine_shader_builder::shader_symbols_builder_configured_for_space_engine;
-
 fn main() -> Result<(), Box<dyn Error>> {
-	shader_symbols_builder_configured_for_space_engine("rust-gpu-bindless-egui-shaders").build()?;
+	ShaderSymbolsBuilder::new("egui-shaders", "spirv-unknown-vulkan1.2").build()?;
 	Ok(())
 }
