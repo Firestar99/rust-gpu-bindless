05e657be428192d6a059f9c99ff2141b100ab64d
Firestar99 <firestar99@sydow.cloud>
2025-03-26 12:01:47 +0100

    core splitup: unify shader capabilities to prevent recompiles

--- tests/integration-test/build.rs
+++ tests/integration-test/build.rs
@@ -1,19 +1,6 @@
-use rust_gpu_bindless_shader_builder::spirv_builder::{Capability, ShaderPanicStrategy, SpirvMetadata};
 use rust_gpu_bindless_shader_builder::ShaderSymbolsBuilder;
 
 fn main() -> anyhow::Result<()> {
-	ShaderSymbolsBuilder::new("integration-test-shader", "spirv-unknown-vulkan1.2")
-		.capability(Capability::GroupNonUniform)
-		.capability(Capability::GroupNonUniformBallot)
-		.capability(Capability::StorageImageExtendedFormats)
-		.capability(Capability::StorageImageReadWithoutFormat)
-		.capability(Capability::StorageImageWriteWithoutFormat)
-		.capability(Capability::ShaderNonUniform)
-		.spirv_metadata(SpirvMetadata::Full)
-		.shader_panic_strategy(ShaderPanicStrategy::DebugPrintfThenExit {
-			print_inputs: true,
-			print_backtrace: true,
-		})
-		.build()?;
+	ShaderSymbolsBuilder::new("integration-test-shader", "spirv-unknown-vulkan1.2").build()?;
 	Ok(())
 }
